deae6f1b16f1c9294cdf36347fe66406681d4794
brdimitrius <47978458+brdimitrius@users.noreply.github.com>
2019-04-03 22:20:06 -0300

    Update cor_teste1.py

--- cor_teste1.py
+++ cor_teste1.py
@@ -1,155 +1,112 @@
 import sys
-# import the necessary packages
-import numpy as np
-import imutils
+import subprocess as sp
 import cv2
+import numpy
+import time
 
-# initialize the list of reference points and boolean indicating
-# whether cropping is being performed or not
-x_start, y_start, x_end, y_end = 0, 0, 0, 0
-recorte = False #cropping
-obterROI = False #getRPO
-refPt = []
-lower = np.array([])
-upper = np.array([])
-
-camera = cv2.VideoCapture(-1)
-
-
-def obter_valor_cor(evento, x, y, flags, param): #click_and_crop # event
-    # grab references to the global variables
-    global x_start, y_start, x_end, y_end, recorte, obterROI
-
-    # if the left mouse button was clicked, record the starting
-    # (x, y) coordinates and indicate that cropping is being
-    # performed
-    if evento == cv2.EVENT_LBUTTONDOWN:
-        x_start, y_start, x_end, y_end = x, y, x, y
-        recorte = True
-
-    elif evento == cv2.EVENT_MOUSEMOVE:
-        if recorte == True:
-            x_end, y_end = x, y
-
-    # check to see if the left mouse button was released
-    elif evento == cv2.EVENT_LBUTTONUP:
-        # record the ending (x, y) coordinates and indicate that
-        # the cropping operation is finished
-        x_end, y_end = x, y
-        recorte = False
-        obterROI = True
-
-
-cv2.namedWindow("imagem")
-cv2.setMouseCallback("imagem", obter_valor_cor)
-
-# keep looping
+# Criando um objeto. 0 = Camera Externa
+#video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(-1)
+video.set(3, 640)
+video.set(4, 360)
+#video.set(5, 60)
+#video.set(4, 360)
+#capture.set(5, 20) #set FPS
+video.set(cv2.CAP_PROP_FPS, 60)
+x_last = 320
+y_last = 180
+a = "a"#90
 while True:
-
-    if not obterROI:
-
-        while True:
-            # grab the current frame
-            (status, frame) = camera.read()
-
-            if not status:
-                break
-
-            if not recorte and not obterROI:
-                cv2.imshow("imagem", frame)
-
-            elif recorte and not obterROI:
-                cv2.rectangle(frame, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
-                cv2.imshow("imagem", frame)
-
-            elif not recorte and obterROI:
-                cv2.rectangle(frame, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
-                cv2.imshow("imagem", frame)
-                break
-
-            key = cv2.waitKey(1) & 0xFF
-            # if the 'q' key is pressed, stop the loop
-            if key == ord("q"):
-                sys.exit()
-                #noROI = True
-        # if there are two reference points, then crop the region of interest
-        # from teh image and display it
-        refPt = [(x_start, y_start), (x_end, y_end)]
-
-        roi = frame[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-        #cv2.imshow("ROI", roi)
-
-        hsvRoi = cv2.cvtColor(roi, cv2.COLOR_RGB2HSV)
-        print('minimo H = {}, minimo S = {}, minimo V = {} \nmaximo H = {}, maximo S = {}, maximo V = {}'.format(hsvRoi[:,:,0].min(), hsvRoi[:,:,1].min(), hsvRoi[:,:,2].min(), hsvRoi[:,:,0].max(), hsvRoi[:,:,1].max(), hsvRoi[:,:,2].max()))
-        lower = np.array([hsvRoi[:, :, 0].min(), hsvRoi[:, :, 1].min(), hsvRoi[:, :, 2].min()])
-        upper = np.array([hsvRoi[:, :, 0].max(), hsvRoi[:, :, 1].max(), hsvRoi[:, :, 2].max()])
-        minimo_H = (hsvRoi[:, :, 0].min())
-        minimo_S = (hsvRoi[:, :, 1].min())
-        minimo_V = (hsvRoi[:, :, 2].min())
-        maximo_H = (hsvRoi[:, :, 0].max())
-        maximo_S = (hsvRoi[:, :, 1].max())
-        maximo_V = (hsvRoi[:, :, 2].max())
-
-        # grab the current frame
-    (status, frame) = camera.read()
-
-    if not status:
+    successful, image = video.read()
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV )
+    cortada = hsv[200:250, 0:639] # Cortar a imagem em forma de retangulo
+    linha_preta = cv2.inRange(image, (0, 0, 0), (50, 50, 50))
+    sinal_verde = cv2.inRange(cortada, (37, 246, 43), (60, 255, 255))
+    kernel = numpy.ones((3, 3), numpy.uint8)
+    linha_preta = cv2.erode(linha_preta, kernel, iterations=5)
+    linha_preta = cv2.dilate(linha_preta, kernel, iterations=9)
+    sinal_verde = cv2.erode(sinal_verde, kernel, iterations=5)
+    sinal_verde = cv2.erode(sinal_verde, kernel, iterations=9)
+    contours_preto, hierarquia_preto = cv2.findContours(linha_preta.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_verde, hierarquia_verde = cv2.findContours(sinal_verde.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    Verde_detectado = False
+    if len(contours_verde) > 0:
+        Verde_detectado = True
+        x_verde, y_verde, w_verde, h_verde = cv2.boundingRect(contours_verde[0])
+        centerx_verde0 = x_verde + (w_verde / 2)
+        print(centerx_verde0)
+        centerx_verde = (int(centerx_verde0))
+        cv2.line(cortada, (centerx_verde, 200), (centerx_verde, 250), (0, 255, 0), 3)
+        setpoint2 = 320
+        error2 = int(x_verde - setpoint2)
+        cv2.putText(image,("Posicao verde (%s)" %(error2)), (30, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    contours_preto_len = len(contours_preto)
+    if contours_preto_len > 0:
+        if contours_preto_len == 1:
+            area_preta = cv2.minAreaRect(contours_preto[0])
+        else:
+            candidatos = []
+            botao_off = 0
+            for con_num in range(contours_preto_len):
+                area_preta = cv2.minAreaRect(contours_preto[con_num])
+                (x_minimo, y_minimo), (w_minimo, h_minimo), angulo = area_preta
+                box = cv2.boxPoints(area_preta)
+                (x_box, y_box) = box[0]
+                x_preto, y_preto, w_preto, h_preto = cv2.boundingRect(contours_preto[0])
+                centerx_preto0 = x_preto + (w_preto / 2)
+                centerx_preto = (int(centerx_preto0))
+                if y_box > 358:
+                    botao_off += 1
+                candidatos.append((y_box, con_num, x_minimo, y_minimo))
+            candidatos = sorted(candidatos)
+            if botao_off > 1:
+                candidatos_botao_off = []
+                for con_num in range((contours_preto_len - botao_off),  contours_preto_len):
+                    (y_maior, con_maior, x_minimo, y_minimo) = candidatos[con_num]
+                    distancia_total = (abs(x_minimo - x_last)** 2 + abs(y_minimo - y_last) ** 2) ** 0.5
+                    candidatos_botao_off.append((distancia_total, con_maior))
+                candidatos_botao_off = sorted(candidatos_botao_off)
+                (distancia_total, con_maior) = candidatos_botao_off[0]
+                area_preta = cv2.minAreaRect(contours_preto[con_maior])
+            else:
+                (y_maior, con_maior, x_minimo, y_minimo) = candidatos[contours_preto_len - 1]
+                area_preta = cv2.minAreaRect(contours_preto[con_maior])
+        (x_minimo, y_minimo), (w_minimo, h_minimo), angulo = area_preta
+        x_last = x_minimo
+        y_last = y_minimo
+        if angulo < -45:
+            angulo = 90 + angulo
+        if w_minimo < h_minimo and angulo > 0:
+            angulo = (90-angulo) * -1
+        if w_minimo > h_minimo and angulo < 0:
+            angulo = 90 + angulo
+        setpoint = 320
+        error = int(x_minimo - setpoint)
+        angulo = int(angulo)
+        box = cv2.boxPoints(area_preta)
+        box = numpy.int0(box)
+        Preto = True
+        cv2.drawContours(image, [box], 0, (0,0,255), 3)
+        cv2.putText(image, str(angulo), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(image, ("Posicao preta (%s)" %(error)), (10, 320), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+        cv2.line(image, (int(x_minimo), 200), (int(x_minimo), 250), (255, 0, 0), 3)
+        #cv2.line(image, (centerx_preto, 200), (centerx_preto, 250), (255, 0, 0), 3)
+        if Preto == True:
+            print("")
+    if Verde_detectado == True:
+        if error2 > 0:
+            Direita = True
+            cv2.putText(image, "Vire a direita", (350,180), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 3)
+        elif error2 < 0:
+            Esquerda = True
+            cv2.putText(image, "Vire a esquerda", (50, 180), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3)
+            #elif len(contours_verde) == 0 and len(contours_preto) == 0
+    else:
+        setpoint = 320
+        #error = centerx_preto - setpoint
+    cv2.imshow('Imagem sem hsv', image)
+    #cv2.imshow("Mascara preta", mask_black)
+    #cv2.imshow("Imagem cortada em hsv", cortada)
+    cv2.imshow("roi", cortada)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    # frame = imutils.resize(frame, width=600)
-
-    borrao = cv2.GaussianBlur(frame, (11, 11), 0) #blurred
-    hsv = cv2.cvtColor(borrao, cv2.COLOR_BGR2HSV)
-
-    # construct a mask for the color from dictionary`1, then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    kernel = np.ones((9, 9), np.uint8)
-    mascara = cv2.inRange(hsv, lower, upper)
-    mascara = cv2.morphologyEx(mascara, cv2.MORPH_OPEN, kernel)
-    mascara = cv2.morphologyEx(mascara, cv2.MORPH_CLOSE, kernel)
-
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    linha = cv2.findContours(mascara.copy(), cv2.RETR_EXTERNAL, #cnts
-                            cv2.CHAIN_APPROX_SIMPLE)[-2]
-    centro = None
-
-    # only proceed if at least one contour was found
-    if len(linha) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(linha, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-        # only proceed if the radius meets a minimum size. Correct this value for your obect's size
-        if radius > 0.5:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 0), 2)
-            cv2.putText(frame, 'center: {}, {}'.format(int(x), int(y)), (int(x - radius), int(y - radius)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-
-    # show the frame to our screen
-    lower_verde = np.array([minimo_H, minimo_S, minimo_V])
-    upper_verde = np.array([maximo_V, maximo_S, maximo_V])
-    mask = cv2.inRange(hsvRoi, lower_verde, upper_verde)
-    res = cv2.bitwise_and(hsvRoi, hsvRoi, mask=mask)
-    cv2.imshow("mask", mask)
-    cv2.imshow("res", res)
-    cv2.imshow("resultado", frame)
-    key = cv2.waitKey(1) & 0xFF
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        break
-    elif key == ord("r"):
-        getROI = False
-
-# cleanup the camera and close any open windows
-camera.release()
-cv2.destroyAllWindows()
